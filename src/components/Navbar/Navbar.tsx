11b42a50d105987ade35ab0e46cf3618c8b20a13
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2023-11-16 15:58:51 +0100

    feat:navbar component completely responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/assets/logo.png";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [isSelectTagActive, setSelectTagActive] = useState(false);
+  const [isMobileSelectTagActive, setMobileSelectTagActive] = useState(false);
+  const [isOverlayFilterActive, setOverlayFilterActive] = useState(false);
 
-
-  
   return (
     <div className="nav-parent">
       <div className="navmenu-wrapper">
@@ -32,14 +33,24 @@ const Navbar = () => {
             </div>
           </div>
 
-          <div className="filter-parent">
+          <div
+            className="filter-parent"
+            onClick={() => setSelectTagActive(!isSelectTagActive)}
+          >
             <div className="inner-filter-content">
               <i className="fa fa-map-marker"></i>
               <span className="filter-placeholder">filter by location...</span>
             </div>
-            <select name="" id="" className="select-choice">
-              <option value="">filter by location</option>
-            </select>
+            {isSelectTagActive && (
+              <div className="select-choice">
+                <span>filter by location</span>
+                <span>filter by location</span>
+                <span>filter by location</span>
+                <span>filter by location</span>
+                <span>filter by location</span>
+                <span>filter by location</span>
+              </div>
+            )}
           </div>
 
           <div className="job-descp">
@@ -50,12 +61,60 @@ const Navbar = () => {
 
             <button className="search-btn">search</button>
             <div className="mobile-seach-pack">
-              <i className="fas fa-filter"></i>
+              <i
+                className="fas fa-filter"
+                onClick={() => setOverlayFilterActive(true)}
+              ></i>
               <button className="search-btn-icon">
                 <i className="fas fa-search"></i>
               </button>
             </div>
           </div>
+
+          {isOverlayFilterActive && (
+            <div
+              className="filter-overlay"
+              onClick={() => setOverlayFilterActive(false)}
+            >
+              <div
+                className="white-content"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                <div
+                  className="top-part"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setMobileSelectTagActive(!isMobileSelectTagActive);
+                  }}
+                >
+                  <i className="fa fa-map-marker"></i>
+                  <span className="filter-placeholder">
+                    filter by location...
+                  </span>
+                  {isMobileSelectTagActive && (
+                    <div className="select-choice">
+                      <span>filter by location</span>
+                      <span>filter by location</span>
+                      <span>filter by location</span>
+                      <span>filter by location</span>
+                      <span>filter by location</span>
+                      <span>filter by location</span>
+                    </div>
+                  )}
+                </div>
+
+                <div className="full-time-filter">
+                  <input type="checkbox" className="mobile-checkbox" />
+                  <span className="fulltime-title">full time only</span>
+                </div>
+                <div className="search-btn-container-second">
+                  <button className="search-btn-mobile">search</button>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
